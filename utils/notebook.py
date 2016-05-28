16e19a6a29beb25f39db8bd4324042f7d0f282fa
Christian Kellner <christian@kellner.me>
2016-05-28 21:48:27 +0900

    [utils] notebook: print_stats can handle 'None'

--- utils/notebook.py
+++ utils/notebook.py
@@ -2,6 +2,9 @@ __author__ = 'andrey'
 
 
 def print_stats(items):
+    if items is None or len(items) < 1:
+        return
+
     print("\n%-50s (%02d)" % (items[0].__class__.__name__ + "s", len(items)))
     for t in set(i.type for i in items):
         print("\ttype: %-35s  (%02d)" % (t, len([1 for i in items if i.type == t])))
